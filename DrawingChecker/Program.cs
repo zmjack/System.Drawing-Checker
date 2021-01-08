de574b7a88c564b3843713c1c31fcf3e2b604444
zmjack <jack@zmland.com>
2021-01-08 15:02:38 +0800

    202101081502

---
+++ DrawingChecker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+
+namespace DrawingChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bitmap = new Bitmap(1, 1))
+            using (var graphics = Graphics.FromImage(new Bitmap(1, 1)))
+            {
+                var fontSize = graphics.MeasureString("Drawing", new Font("Consolas", 11));
+                var width = fontSize.Width;
+                Console.WriteLine(width);
+            }
+        }
+    }
+}
